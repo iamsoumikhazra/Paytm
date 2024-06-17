259c64dc603ce3860171cd6796b0f7037fb57ab7
iamsoumikhazra <www.soumikhazra@gmail.com>
2024-06-17 21:31:39 +0530

    setup complete

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -5,6 +5,7 @@ dotenvConfig();
 const _config={
   port: process.env.PORT || 5000,
   dbUrl: process.env.MONGO_URL,
+  env: process.env.NODE_ENV,
 };
 
 export const config = Object.freeze(_config);
